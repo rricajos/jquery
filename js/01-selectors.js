52644a57f0f7ada30cc9563b3fcb3356ceda9633
Ricard Penin Honrubia <ergoegos@gmail.com>
2024-02-29 17:35:45 +0100

    02-events
    and some ui responsive upgrades

--- js/01-selectors.js
+++ js/01-selectors.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
   console.log("Document ready - Load succsessfully !");
 
+  // The .html() method sets all the h1 elements' html to be "hello world":
+  $("h1").html("hello jquery 3.7  ");
+
   // select element with id red
   $("#red")
     .css("background", "red")
@@ -19,4 +22,8 @@ $(document).ready(function () {
     .css("background", "green")
     .css("color", "white")
 
+  // select elements with atribute href='somewhere.com'
+  $("[href='somewhere.com']").css('background', '#eee');
+
+  
 }) 
---
+++ js/02-events.js
@@ -0,0 +1,114 @@
+$(document).ready(function () {
+  // Mouse over and out
+  // $("#02-events").mouseover(function () {
+  //   $(this).css("background", "#ADD8E6");
+  // });
+  // $("#02-events").mouseout(function () {
+  //   $(this).css("background", "#fff");
+  // });
+
+  // hover for elements starting with id 02-events*
+  $("[id^='02-events']").hover(
+    // param 1 : mouseover
+    function () {
+      $(this).css("background", "#ADD8E6");
+      $(this).css("cursor", "pointer")
+    },
+    // param 2 : mouseout
+    function () {
+      $(this).css("background", "#fff");
+    }
+  );
+
+  // mousemove
+  $(document).mousemove(function () {
+
+    $("#02-events > p").text("mousemove event: "+ event.pageX + " X·Y " + event.pageY);
+  });
+
+  // dbl click (also for touch events.)
+  $("[id^='02-events']").click(function () {
+    $(document).off("mousemove");
+    $(this).css("background", "yellow");
+  });
+  // dbl click handle
+  $("[id^='02-events']").dblclick(function () {
+   
+    $(this).addClass("bgyellow");
+  });
+
+
+
+});
+
+/* 
+
+These are just some examples. 
+There are many other events available, and the 
+list may vary depending on the environment and 
+the object to which you are attaching the event.
+
+
+Mouse Events:
+
+click
+dblclick
+mouseover
+mouseout
+mousedown
+mouseup
+mousemove
+
+Keyboard Events:
+
+keydown
+keyup
+keypress
+
+Form Events:
+
+submit
+reset
+change
+focus
+blur
+input
+
+Window Events:
+
+load
+unload
+resize
+scroll
+
+Document Events:
+
+DOMContentLoaded
+readystatechange
+
+Navigation Events:
+
+hashchange
+beforeunload
+popstate
+
+Animation Events:
+
+transitionend
+animationstart
+animationend
+animationiteration
+
+Drag and Drop Events:
+
+dragstart
+drag
+dragenter
+dragleave
+dragover
+drop
+dragend
+
+
+
+*/
